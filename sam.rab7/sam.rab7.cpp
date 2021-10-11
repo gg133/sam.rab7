1ad919676663cdb074274b086ba181fa70564ebd
gg133 <133chess@gmail.com>
2021-10-11 15:45:25 +0300

    7,8,9

--- sam.rab7/sam.rab7.cpp
+++ sam.rab7/sam.rab7.cpp
@@ -5,16 +5,20 @@ using namespace std;
 int main()
 {
 	setlocale(0, "");
-	cout << "\"Геометрические фигуры\"\n\nВыберите фигуру: \n1.Линия\n2.Квадрат\n3.Выход\n";
+	cout << "\"Геометрические фигуры\"\n\nВыберите фигуру: \n1.Линия\n2.Квадрат\n3.Прямоугольник\n4.Треугольник\n5.Выход\n";
 	int menu; 
 	int size; 
 	char texture; 
 	int place; 
 	int a = 0; 
 	int a1 = 0; 
+	int size2;
+	int cursed;
+	int cursed2;
 	cin >> menu;
 	switch (menu)
 	{
+		//ЛИНИЯ
 	case 1:
 		system("cls");
 		cout << "Укажите параметры линии\n1.Горизонтальная\n2.Вертикальная\n";
@@ -24,14 +28,15 @@ int main()
 		case 1:
 			system("cls");
 			cout << "Введите размер лини\n"; cin >> size;
-				
-			while(cin.fail() || size < 1){
-					cin.clear();
-					cin.ignore();
-					cout << "error/Введите корректные данные(Обычное положительное число)\n";
-					cin >> size;
+
+			while (cin.fail() || size < 1)
+			{
+				cin.clear();
+				cin.ignore();
+				cout << "error/Введите корректные данные(Обычное положительное число)\n";
+				cin >> size;
 			}
-	
+
 			cout << "Введите текстуру линии\n"; cin >> texture;
 			system("cls");
 			cout << "\n";
@@ -40,12 +45,14 @@ int main()
 				a++;
 				cout << texture;
 			}
+			cout << "\n";
 			break;
 		case 2:
 			system("cls");
 			cout << "Укажите размер линии\n";
 			cin >> size;
-			while (cin.fail() || size < 1) {
+			while (cin.fail() || size < 1)
+			{
 				cin.clear();
 				cin.ignore();
 				cout << "error/Введите корректные данные(Обычное положительное число)\n";
@@ -60,6 +67,7 @@ int main()
 				a++;
 				cout << texture << "\n";
 			}
+			cout << "\n";
 			break;
 
 		default:
@@ -67,29 +75,161 @@ int main()
 			break;
 		}
 		break;
+		//КВАДРАТ
 	case 2:
 		system("cls");
-		cout << "Введите размер лини\n"; cin >> size;
-		while (cin.fail() || size < 1) {
-			cin.clear();
-			cin.ignore();
-			cout << "error/Введите корректные данные(Обычное положительное число)\n";
-			cin >> size;
-		}
-		cout << "Введите текстуру линии\n"; cin >> texture;
-		a = size;
-		while (a != 0)
+		cout << "Выберие тип квадрата\n\n1.Пустой\n2.Закрашенный\n";
+		cin >> cursed;
+		switch (cursed)
 		{
-			a1 = size;
-			while (a1 != 0) {
-				cout << texture << " ";
-				a1--;
+
+		case 1:
+			system("cls");
+			cout << "Введите размер лини\n"; cin >> size;
+			while (cin.fail() || size < 1)
+			{
+				cin.clear();
+				cin.ignore();
+				cout << "error/Введите корректные данные(Обычное положительное число)\n";
+				cin >> size;
 			}
-			cout << "\n";
-			a--;
+			cout << "Введите текстуру линии\n"; cin >> texture;
+			a = size;
+			while (a != 0)
+			{
+				a1 = size;
+				while (a1 != 0)
+				{
+					if (a == 1 || a == size || a1 == 1 || a1 == size)
+						cout << texture << " ";
+					else
+						cout << "  ";
+					a1--;
+				}
+				cout << "\n";
+				a--;
+			}
+			break;
+		case 2:
+			system("cls");
+			cout << "Введите размер лини\n"; cin >> size;
+			while (cin.fail() || size < 1)
+			{
+				cin.clear();
+				cin.ignore();
+				cout << "error/Введите корректные данные(Обычное положительное число)\n";
+				cin >> size;
+			}
+			cout << "Введите текстуру линии\n"; cin >> texture;
+			a = size;
+			while (a != 0)
+			{
+				a1 = size;
+				while (a1 != 0)
+				{
+					cout << texture << " ";
+					a1--;
+				}
+				cout << "\n";
+				a--;
+			}
+			break;
+		default:
+			cout << "Такого варианта не существует, перезапустите программу\n";
+			break;
 		}
 		break;
-	case  3:
+		//ПРЯМОУГОЛЬНИК
+	case 3:
+		system("cls");
+		cout << "Выберите прямоугольник\n\n1.Закрашенный.2.Пустой\n";
+		cin >> cursed2;
+		switch (cursed2)
+		{
+		case 1:
+			system("cls");
+			cout << "Введите размер горизонтальной  лини\n";
+			cin >> size;
+			cout << "Введите размер вертикальной лини\n";
+			cin >> size2;
+
+			while (cin.fail() || size < 1) 
+			{
+				cin.clear();
+				cin.ignore();
+				cout << "error/Введите корректные данные(Обычное положительное число)\n";
+				cin >> size;
+			}
+			cout << "Введите текстуру линии\n"; cin >> texture;
+			a = 0;
+			while (a != size2)
+			{
+				a1 = 0;
+				while (a1 != size)
+				{
+					cout << texture << " ";
+					a1++;
+				}
+				cout << "\n";
+				a++;
+			}
+			break;
+		
+		case 2:
+				system("cls");
+				cout << "Введите размер горизонтальной  лини\n";
+				cin >> size;
+				cout << "Введите размер вертикальной лини\n";
+				cin >> size2;
+
+				while (cin.fail() || size < 1) {
+					cin.clear();
+					cin.ignore();
+					cout << "error/Введите корректные данные(Обычное положительное число)\n";
+					cin >> size;
+				}
+				cout << "Введите текстуру линии\n";
+				cin >> texture;
+				cout << "\n";
+				a = size2;
+				while (a != 0)
+				{
+					a1 = size;
+					while (a1 != 0)
+					{
+						if (a == 1 || a == size2 || a1 == 1 || a1 == size)
+						{
+							cout << texture << " ";
+						}
+						else {
+							cout << "  ";
+						}
+						a1--;
+					}
+					cout << "\n";
+					a--;
+				}
+				break;
+			default:
+				cout << "Такого варианта нет\n";
+				break;
+		}
+		break;
+		//ТРЕУГОЛЬНИК
+		case 4:
+			system("cls");
+			cout << "Введите размер лини\n"; cin >> size;
+			while (cin.fail() || size < 1) {
+				cin.clear();
+				cin.ignore();
+				cout << "error/Введите корректные данные(Обычное положительное число)\n";
+				cin >> size;
+			}
+			cout << "Введите текстуру линии\n"; cin >> texture;
+			a = size;
+
+		break;
+	case  5:
 		system("cls");
 		cout << "Вы вышли из программы\n";
 		system("pause");
